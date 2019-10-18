87ae4c86a1f5791290d7420078d1ee962b753068
Alexander Moreno <ahweyalex@gmail.com>
2019-10-18 11:08:00 -0500

    Update (10/18)
    working on the rect wire function

--- Matlab/BFields_Wire_Antenna_UI/constrWireAntRect.m
+++ Matlab/BFields_Wire_Antenna_UI/constrWireAntRect.m
@@ -1,5 +1,15 @@
+clear all; close all; clc;
 
-function [xS,yS,zS] = constrWireAntRect(h,W0,L0,N,O,wT)
-    deltaS   = 200;
-    w = W0*ones(10,1);
-end
+deltaS = 200; N=2; phi=2;
+helixSTEP = phi*(pi/180);
+start=0; fin = N*(2*pi) + helixSTEP/2;
+cst_xxx = start:helixSTEP:fin;
+cstSize = floor(numel(cst_xxx)/4);
+W0 = 2; L0 = 6; 
+
+x = linspace(-W0/2,W0/2,cstSize); 
+y = linspace(-L0/2,L0/2,cstSize);
+
+%%
+xS0 = cat(1,W0*ones(floor(deltaS/4),1),L0*ones(floor(deltaS/4),1),-W0*ones(floor(deltaS/4),1),-L0*ones(floor(deltaS/4),1));
+yS0 = cat(1,W0*ones(floor(deltaS/4),1),L0*ones(floor(deltaS/4),1),-W0*ones(floor(deltaS/4),1),-L0*ones(floor(deltaS/4),1));
