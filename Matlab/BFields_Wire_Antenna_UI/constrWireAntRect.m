c7acfe52618fd33169eb5cc33ca284b5f04207b3
Alexander Moreno <ahweyalex@gmail.com>
2019-10-21 13:19:47 -0500

    Update: Rect Function
    got an initial rect wire
    need to fix it:
    make it rect right not its octogaon
    not starting or ending correctly

--- Matlab/BFields_Wire_Antenna_UI/constrWireAntRect.m
+++ Matlab/BFields_Wire_Antenna_UI/constrWireAntRect.m
@@ -1,15 +1,29 @@
 clear all; close all; clc;
 
-deltaS = 200; N=2; phi=2;
+ra=0.3; ri=0.3; phi=2; N=2; O=1; wT=0.1; h=(1.1)*(2*wT*N);
+W0=2; L0=6; 
+
+deltaS = 200; 
 helixSTEP = phi*(pi/180);
 start=0; fin = N*(2*pi) + helixSTEP/2;
 cst_xxx = start:helixSTEP:fin;
-cstSize = floor(numel(cst_xxx)/4);
-W0 = 2; L0 = 6; 
+cstSize = floor((numel(cst_xxx)/4)/N);
 
-x = linspace(-W0/2,W0/2,cstSize); 
-y = linspace(-L0/2,L0/2,cstSize);
+x = linspace(-W0/2,W0/2,cstSize)'; xc = W0*ones(1,cstSize)';
+y = linspace(-L0/2,L0/2,cstSize)'; yc = L0*ones(1,cstSize)';
+x0 = [x;xc;flipud(x);-xc];           y0 = [-yc;y;yc;flipud(y)];
+
+xS0 = repmat(x0,N,1);
+yS0 = repmat(y0,N,1);
+
+zSize = numel(xS0);
+zS0 = linspace(start,fin, zSize)';
 
 %%
-xS0 = cat(1,W0*ones(floor(deltaS/4),1),L0*ones(floor(deltaS/4),1),-W0*ones(floor(deltaS/4),1),-L0*ones(floor(deltaS/4),1));
-yS0 = cat(1,W0*ones(floor(deltaS/4),1),L0*ones(floor(deltaS/4),1),-W0*ones(floor(deltaS/4),1),-L0*ones(floor(deltaS/4),1));
+H1=plot3(xS0,yS0,zS0);
+%H=plot(xS0,yS0);
+title('rect wire');
+xlabel('x'); ylabel('y'); 
+%zlabel('z');
+view(0,90)
+%zS0 = ((h*cst_xxx)./(2*pi*N))';
