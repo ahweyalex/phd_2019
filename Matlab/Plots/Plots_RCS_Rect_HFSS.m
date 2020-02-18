c144465ab18fdc76b9d596ca76992912c70450c8
ahweyalex <ahweyalex@gmail.com>
2020-02-17 21:24:04 -0600

    Includes 2lam by 2lam 45deg

--- Matlab/Plots/Plots_RCS_Rect_HFSS.m
+++ Matlab/Plots/Plots_RCS_Rect_HFSS.m
@@ -8,6 +8,8 @@ fn5 = 'Mono_RCS_Rect_Sband_2lam2lam3Q_GND.csv';
 fn6 = 'Mono_RCS_Rect_Sband_2lam2lam3Q_GND_polPhTh.csv';
 fn7 = 'Mono_RCS_Rect_Sband_2lam2lam3Q_GND_FlipV.csv';
 fn8 = 'Mono_RCS_Rect_Sband_2lam2lam3Q_GND_FlipV_polPhTh.csv';
+fn9 = 'Mono_RCS_Rect_Sband_2lam2lam_GND_F45.csv';
+fn10 = 'Mono_RCS_Rect_Sband_2lam2lam_GND_F45_polPhTh.csv';
 % import data
 % 2 lam by 2 lam 
 twoLam        = importdata(fn1);
@@ -15,6 +17,9 @@ twoLam_pol    = importdata(fn2);
 % 2 lam by 2 lam flip
 twoLamv       = importdata(fn3);
 twoLamv_pol   = importdata(fn4);
+% 2lam by 2lam flip 45
+twoLam45 = importdata(fn9);
+twoLam45_pol = importdata(fn10);
 % 2 lam by 2 lam3Q
 twoLam3Q      = importdata(fn5);
 twoLam3Q_pol  = importdata(fn6);
@@ -24,6 +29,8 @@ twoLam3Qv_pol = importdata(fn8);
 % extract data
 a= twoLam.data(:,1).*(pi/180);
 % 2 lam by 2 lam
+%tLam_p0     = twoLam.data(:,2);
+%tLam_p90    = twoLam.data(:,3);
 tLam_p0     = 20*log10(twoLam.data(:,2));
 tLam_p90    = 20*log10(twoLam.data(:,3));
 tLam_p0_ph  = 20*log10(twoLam_pol.data(:,2));
@@ -37,6 +44,14 @@ tLamv_p0_ph  = 20*log10(twoLamv_pol.data(:,2));
 tLamv_p90_ph = 20*log10(twoLamv_pol.data(:,3));
 tLamv_p0_th  = 20*log10(twoLamv_pol.data(:,4));
 tLamv_p90_th = 20*log10(twoLamv_pol.data(:,5));
+% 2lam by 2lam flip 45 degrees
+tLam45_p0     = 20*log10(twoLam45.data(:,2));
+tLam45_p90    = 20*log10(twoLam45.data(:,3));
+tLam45_p0_ph  = 20*log10(twoLam45_pol.data(:,2));
+tLam45_p90_ph = 20*log10(twoLam45_pol.data(:,3));
+tLam45_p0_th  = 20*log10(twoLam45_pol.data(:,4));
+tLam45_p90_th = 20*log10(twoLam45_pol.data(:,5));
+
 % 2 lam by 2 lam3Q
 tLam3Q_p0     = 20*log10(twoLam3Q.data(:,2));
 tLam3Q_p90    = 20*log10(twoLam3Q.data(:,3));
@@ -55,17 +70,19 @@ tLam3Qv_p90_th = 20*log10(twoLam3Qv_pol.data(:,5));
 % 2lam by 2lam
 % total RCS
 figure(1)
-H = mmpolar(a,tLam_p0,a,tLamv_p0,...
+H = mmpolar(a,tLam_p0,a,tLamv_p0,a,tLam45_p0,...
     'Style','compass','TGridVisible','off',...
     'TTickOffset',0.05,'TTickDelta',30);
 set(H,'LineWidth',3);
-set(H(1),'color','r'); set(H(2),'color','b');
+set(H(1),'color','r'); set(H(2),'color','b'); 
 set(H(1),'LineStyle','-'); set(H(2),'LineStyle','--'); 
+set(H(3),'color','k'); set(H(3),'LineStyle','-');
 fig=gcf; set(findall(fig,'-property','Fontsize'),'Fontsize',14); grid off;
 title('Monostatic RCS: 2 \lambda by 2 \lambda \phi:0^\circ');
-legend({'pos:horizontal','pos:vertical'},'Location','northwest');
+% legend({'pos:horizontal','pos:vertical'},'Location','northwest');
+legend({'pos:horizontal','pos:vertical','pos:45^\circ'},'Location','northwest');
 figure(2)
-H = mmpolar(a,tLam_p0,a,tLamv_p0,...
+H = mmpolar(a,tLam_p90,a,tLamv_p90,a,tLam45_p90,...
     'Style','compass','TGridVisible','off',...
     'TTickOffset',0.05,'TTickDelta',30);
 set(H,'LineWidth',3);
@@ -73,11 +90,13 @@ set(H(1),'color','r'); set(H(2),'color','b');
 set(H(1),'LineStyle','-'); set(H(2),'LineStyle','--'); 
 fig=gcf; set(findall(fig,'-property','Fontsize'),'Fontsize',14); grid off;
 title('Monostatic RCS: 2 \lambda by 2 \lambda \phi:90^\circ');
-legend({'pos:horizontal','pos:vertical'},'Location','northwest');
+% legend({'pos:horizontal','pos:vertical'},'Location','northwest');
+legend({'pos:horizontal','pos:vertical','pos:45^\circ'},'Location','northwest');
+set(H(3),'color','k'); set(H(3),'LineStyle','-');
 
-% pol: phi
+%% pol: phi
 figure(3)
-H = mmpolar(a,tLam_p0_ph,a,tLamv_p0_ph,...
+H = mmpolar(a,tLam_p0_ph,a,tLamv_p0_ph,a,tLam45_p0_ph,...
     'Style','compass','TGridVisible','off',...
     'TTickOffset',0.05,'TTickDelta',30);
 set(H,'LineWidth',3);
@@ -85,9 +104,12 @@ set(H(1),'color','r'); set(H(2),'color','b');
 set(H(1),'LineStyle','-'); set(H(2),'LineStyle','--'); 
 fig=gcf; set(findall(fig,'-property','Fontsize'),'Fontsize',14); grid off;
 title('Monostatic RCS: 2 \lambda by 2 \lambda  pol:\phi \phi:0^\circ');
-legend({'pos:horizontal','pos:vertical'},'Location','northwest');
+% legend({'pos:horizontal','pos:vertical'},'Location','northwest');
+legend({'pos:horizontal','pos:vertical','pos:45^\circ'},'Location','northwest');
+set(H(3),'color','k'); set(H(3),'LineStyle','-');
+
 figure(4)
-H = mmpolar(a,tLam_p90_ph,a,tLamv_p90_ph,...
+H = mmpolar(a,tLam_p90_ph,a,tLamv_p90_ph,a,tLam45_p90_ph,...
     'Style','compass','TGridVisible','off',...
     'TTickOffset',0.05,'TTickDelta',30);
 set(H,'LineWidth',3);
@@ -95,10 +117,13 @@ set(H(1),'color','r'); set(H(2),'color','b');
 set(H(1),'LineStyle','-'); set(H(2),'LineStyle','--'); 
 fig=gcf; set(findall(fig,'-property','Fontsize'),'Fontsize',14); grid off;
 title('Monostatic RCS: 2 \lambda by 2 \lambda  pol:\phi \phi:90^\circ');
-legend({'pos:horizontal','pos:vertical'},'Location','northwest');
-% pol: theta
+% legend({'pos:horizontal','pos:vertical'},'Location','northwest');
+legend({'pos:horizontal','pos:vertical','pos:45^\circ'},'Location','northwest');
+set(H(3),'color','k'); set(H(3),'LineStyle','-');
+
+%% pol: theta
 figure(5)
-H = mmpolar(a,tLam_p0_th,a,tLamv_p0_th,...
+H = mmpolar(a,tLam_p0_th,a,tLamv_p0_th,a,tLam45_p0_th,...
     'Style','compass','TGridVisible','off',...
     'TTickOffset',0.05,'TTickDelta',30);
 set(H,'LineWidth',3);
@@ -106,9 +131,12 @@ set(H(1),'color','r'); set(H(2),'color','b');
 set(H(1),'LineStyle','-'); set(H(2),'LineStyle','--'); 
 fig=gcf; set(findall(fig,'-property','Fontsize'),'Fontsize',14); grid off;
 title('Monostatic RCS: 2 \lambda by 2 \lambda  pol:\theta \phi:0^\circ');
-legend({'pos:horizontal','pos:vertical'},'Location','northwest');
+% legend({'pos:horizontal','pos:vertical'},'Location','northwest');
+legend({'pos:horizontal','pos:vertical','pos:45^\circ'},'Location','northwest');
+set(H(3),'color','k'); set(H(3),'LineStyle','-');
+
 figure(6)
-H = mmpolar(a,tLam_p90_th,a,tLamv_p90_th,...
+H = mmpolar(a,tLam_p90_th,a,tLamv_p90_th,a,tLam45_p90_th,...
     'Style','compass','TGridVisible','off',...
     'TTickOffset',0.05,'TTickDelta',30);
 set(H,'LineWidth',3);
@@ -116,5 +144,8 @@ set(H(1),'color','r'); set(H(2),'color','b');
 set(H(1),'LineStyle','-'); set(H(2),'LineStyle','--'); 
 fig=gcf; set(findall(fig,'-property','Fontsize'),'Fontsize',14); grid off;
 title('Monostatic RCS: 2 \lambda by 2 \lambda  pol:\theta \phi:90^\circ');
-legend({'pos:horizontal','pos:vertical'},'Location','northwest');
+% legend({'pos:horizontal','pos:vertical'},'Location','northwest');
+legend({'pos:horizontal','pos:vertical','pos:45^\circ'},'Location','northwest');
+set(H(3),'color','k'); set(H(3),'LineStyle','-');
+
 %}
