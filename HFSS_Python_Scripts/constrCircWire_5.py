eaaf55391dbc6b9166cf4db6e000ff985cf3fede
Alexander Moreno <ahweyalex@gmail.com>
2020-04-19 02:33:03 -0500

    updates python scripts

--- HFSS_Python_Scripts/constrCircWire_5.py
+++ HFSS_Python_Scripts/constrCircWire_5.py
@@ -18,8 +18,8 @@ for x in range(0,(nxySize)*2,2):
 	Nxy.append(str(x))
 	
 wT = "0.2546mm"
-ra = "30mm"
-ri = "30mm"
+ra = "0.5mm"
+ri = "0.5mm"
 N  = "3"
 numSeg = "200"
 O = 1
@@ -181,61 +181,62 @@ if O==1:
 						"RotateAngle:="		, "90deg"
 					])
 				# create connecting line
-				oEditor.CreatePolyline(
-					[
-						"NAME:PolylineParameters",
-						"IsPolylineCovered:="	, True,
-						"IsPolylineClosed:="	, False,
+				if n!=(nxySize-1):
+					oEditor.CreatePolyline(
 						[
-							"NAME:PolylinePoints",
+							"NAME:PolylineParameters",
+							"IsPolylineCovered:="	, True,
+							"IsPolylineClosed:="	, False,
 							[
-								"NAME:PLPoint",
-								"X:="			, "0mm",
-								"Y:="			, "ri + wT*"+Nxy[n]+".2",
-								"Z:="			, "0"
+								"NAME:PolylinePoints",
+								[
+									"NAME:PLPoint",
+									"X:="			, "0mm",
+									"Y:="			, "ri + wT*"+Nxy[n]+".2",
+									"Z:="			, "0"
+								],
+								[
+									"NAME:PLPoint",
+									"X:="			, "0mm",
+									"Y:="			, "ri + wT*"+Nxy[n+1]+".2",
+									"Z:="			, "0"
+								]
 							],
 							[
-								"NAME:PLPoint",
-								"X:="			, "0mm",
-								"Y:="			, "ri + wT*"+Nxy[n+1]+".2",
-								"Z:="			, "0"
-							]
-						],
-						[
-							"NAME:PolylineSegments",
+								"NAME:PolylineSegments",
+								[
+									"NAME:PLSegment",
+									"SegmentType:="		, "Line",
+									"StartIndex:="		, 0,
+									"NoOfPoints:="		, 2
+								]
+							],
 							[
-								"NAME:PLSegment",
-								"SegmentType:="		, "Line",
-								"StartIndex:="		, 0,
-								"NoOfPoints:="		, 2
+								"NAME:PolylineXSection",
+								"XSectionType:="	, "None",
+								"XSectionOrient:="	, "Auto",
+								"XSectionWidth:="	, "0mm",
+								"XSectionTopWidth:="	, "0mm",
+								"XSectionHeight:="	, "0mm",
+								"XSectionNumSegments:="	, "0",
+								"XSectionBendType:="	, "Corner"
 							]
-						],
+						], 
 						[
-							"NAME:PolylineXSection",
-							"XSectionType:="	, "None",
-							"XSectionOrient:="	, "Auto",
-							"XSectionWidth:="	, "0mm",
-							"XSectionTopWidth:="	, "0mm",
-							"XSectionHeight:="	, "0mm",
-							"XSectionNumSegments:="	, "0",
-							"XSectionBendType:="	, "Corner"
-						]
-					], 
-					[
-						"NAME:Attributes",
-						"Name:="		, "Polyline"+str(n),
-						"Flags:="		, "",
-						"Color:="		, "(143 175 143)",
-						"Transparency:="	, 0,
-						"PartCoordinateSystem:=", "Global",
-						"UDMId:="		, "",
-						"MaterialValue:="	, "\"vacuum\"",
-						"SurfaceMaterialValue:=", "\"\"",
-						"SolveInside:="		, True,
-						"IsMaterialEditable:="	, True,
-						"UseMaterialAppearance:=", False,
-						"IsLightweight:="	, False
-					])
+							"NAME:Attributes",
+							"Name:="		, "Polyline"+str(n),
+							"Flags:="		, "",
+							"Color:="		, "(143 175 143)",
+							"Transparency:="	, 0,
+							"PartCoordinateSystem:=", "Global",
+							"UDMId:="		, "",
+							"MaterialValue:="	, "\"vacuum\"",
+							"SurfaceMaterialValue:=", "\"\"",
+							"SolveInside:="		, True,
+							"IsMaterialEditable:="	, True,
+							"UseMaterialAppearance:=", False,
+							"IsLightweight:="	, False
+						])
 
 			# no flip
 			# even			
@@ -278,60 +279,134 @@ if O==1:
 						"IsLightweight:="	, False
 					])
 				# create connecting line
-				oEditor.CreatePolyline(
-					[
-						"NAME:PolylineParameters",
-						"IsPolylineCovered:="	, True,
-						"IsPolylineClosed:="	, False,
+				if n!=(nxySize-1):
+					oEditor.CreatePolyline(
 						[
-							"NAME:PolylinePoints",
+							"NAME:PolylineParameters",
+							"IsPolylineCovered:="	, True,
+							"IsPolylineClosed:="	, False,
 							[
-								"NAME:PLPoint",
-								"X:="			, "0mm",
-								"Y:="			, "ri + wT*"+Nxy[n]+".2",
-								"Z:="			, "zEnd"
+								"NAME:PolylinePoints",
+								[
+									"NAME:PLPoint",
+									"X:="			, "0mm",
+									"Y:="			, "ri + wT*"+Nxy[n]+".2",
+									"Z:="			, "zEnd"
+								],
+								[
+									"NAME:PLPoint",
+									"X:="			, "0mm",
+									"Y:="			, "ri + wT*"+Nxy[n+1]+".2",
+									"Z:="			, "zEnd"
+								]
 							],
 							[
-								"NAME:PLPoint",
-								"X:="			, "0mm",
-								"Y:="			, "ri + wT*"+Nxy[n+1]+".2",
-								"Z:="			, "zEnd"
-							]
-						],
-						[
-							"NAME:PolylineSegments",
+								"NAME:PolylineSegments",
+								[
+									"NAME:PLSegment",
+									"SegmentType:="		, "Line",
+									"StartIndex:="		, 0,
+									"NoOfPoints:="		, 2
+								]
+							],
 							[
-								"NAME:PLSegment",
-								"SegmentType:="		, "Line",
-								"StartIndex:="		, 0,
-								"NoOfPoints:="		, 2
+								"NAME:PolylineXSection",
+								"XSectionType:="	, "None",
+								"XSectionOrient:="	, "Auto",
+								"XSectionWidth:="	, "0mm",
+								"XSectionTopWidth:="	, "0mm",
+								"XSectionHeight:="	, "0mm",
+								"XSectionNumSegments:="	, "0",
+								"XSectionBendType:="	, "Corner"
 							]
-						],
+						], 
 						[
-							"NAME:PolylineXSection",
-							"XSectionType:="	, "None",
-							"XSectionOrient:="	, "Auto",
-							"XSectionWidth:="	, "0mm",
-							"XSectionTopWidth:="	, "0mm",
-							"XSectionHeight:="	, "0mm",
-							"XSectionNumSegments:="	, "0",
-							"XSectionBendType:="	, "Corner"
-						]
-					], 
-					[
-						"NAME:Attributes",
-						"Name:="		, "Polyline"+str(n),
-						"Flags:="		, "",
-						"Color:="		, "(143 175 143)",
-						"Transparency:="	, 0,
-						"PartCoordinateSystem:=", "Global",
-						"UDMId:="		, "",
-						"MaterialValue:="	, "\"vacuum\"",
-						"SurfaceMaterialValue:=", "\"\"",
-						"SolveInside:="		, True,
-						"IsMaterialEditable:="	, True,
-						"UseMaterialAppearance:=", False,
-						"IsLightweight:="	, False
-					])
+							"NAME:Attributes",
+							"Name:="		, "Polyline"+str(n),
+							"Flags:="		, "",
+							"Color:="		, "(143 175 143)",
+							"Transparency:="	, 0,
+							"PartCoordinateSystem:=", "Global",
+							"UDMId:="		, "",
+							"MaterialValue:="	, "\"vacuum\"",
+							"SurfaceMaterialValue:=", "\"\"",
+							"SolveInside:="		, True,
+							"IsMaterialEditable:="	, True,
+							"UseMaterialAppearance:=", False,
+							"IsLightweight:="	, False
+						])
+
+# start ccw	
 else:
 	a=1
+	
+########################################################################
+#### END: Combine lines and sweep circle to create 3D object
+#######################################################################
+# oEditor.CreateCircle(
+	# [
+		# "NAME:CircleParameters",
+		# "IsCovered:="		, True,
+		# "XCenter:="		, "0mm",
+		# "YCenter:="		, ri,
+		# "ZCenter:="		, "0mm",
+		# "Radius:="		, wT,
+		# "WhichAxis:="		, "X",
+		# "NumSegments:="		, "0"
+	# ], 
+	# [
+		# "NAME:Attributes",
+		# "Name:="		, "Circle0",
+		# "Flags:="		, "",
+		# "Color:="		, "(143 175 143)",
+		# "Transparency:="	, 0,
+		# "PartCoordinateSystem:=", "Global",
+		# "UDMId:="		, "",
+		# "MaterialValue:="	, "\"vacuum\"",
+		# "SurfaceMaterialValue:=", "\"\"",
+		# "SolveInside:="		, True,
+		# "IsMaterialEditable:="	, True,
+		# "UseMaterialAppearance:=", False,
+		# "IsLightweight:="	, False
+	# ])
+
+cStr = "EquationCurve"
+pStr = "Polyline"
+q    = '"'
+c    = ","
+eqC  = ""
+poL  = ""
+for n in range(nxySize):
+	if n==nxySize:
+		eqC = eqC + cStr +str(n)
+	elif n==(nxySize-1):
+		eqC = eqC + cStr + str(n) + c
+		poL = poL + pStr + str(n)
+	else:
+		eqC = eqC + cStr + str(n) + c
+		poL = pStr + str(n) + c
+
+selNames = q+ eqC + poL +q
+oEditor.Unite(
+	[
+		"NAME:Selections",
+		"Selections:="		, selNames
+	], 
+	[
+		"NAME:UniteParameters",
+		"KeepOriginals:="	, False
+	])
+	
+# oEditor.SweepAlongPath(
+	# [
+		# "NAME:Selections",
+		# "Selections:="		, "EquationCurve0,Circle0",
+		# "NewPartsModelFlag:="	, "Model"
+	# ], 
+	# [
+		# "NAME:PathSweepParameters",
+		# "DraftAngle:="		, "0deg",
+		# "DraftType:="		, "Round",
+		# "CheckFaceFaceIntersection:=", False,
+		# "TwistAngle:="		, "0deg"
+	# ])
