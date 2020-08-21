5ee3765a2824db59a3b2441e58c78695f5ef1f56
ahweyalex <ahweyalex@gmail.com>
2020-08-21 00:49:41 -0500

    Figures: Plot and Save
    Matlab script to save 3 cut planes
    some minor adjustments are need to clean up the figures, figures are saved as pdf

--- Matlab/1_Antenna_UI/export_BF_Ant.m
+++ Matlab/1_Antenna_UI/export_BF_Ant.m
@@ -2,54 +2,181 @@
 % DATE: 08-20-2020
 % DESCRIPTION: Exports
 %
-aSize = numel(Sx); 
-aXYZ = {'x[m]','y[m]','z[m]'};
-for n=1:aSize
-    aXYZ{n+1,1} = Sx(n);
-    aXYZ{n+1,2} = Sy(n);
-    aXYZ{n+1,3} = Sz(n);
-end
-writecell(aXYZ,'antenna.csv');
-%  B-Fields
-bSize = Nx*Ny*Nz;
-Xcsv = reshape(B0.X,bSize,1);
-Ycsv = reshape(B0.Y,bSize,1);
-Zcsv = reshape(B0.Z,bSize,1);
-BXcsv = reshape(B0.BX,bSize,1);
-BYcsv = reshape(B0.BY,bSize,1);
-BZcsv = reshape(B0.BZ,bSize,1);
-BFC   = {'x[m]','y[m]','z[m]','BX','BY','BZ'};
-BFA   = num2cell([Xcsv,Ycsv,Zcsv,BXcsv,BYcsv,BZcsv]);
-BF    = [BFC;BFA];
-writecell(aXYZ,'BFields_AllPoints.csv');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% XY
-XYN       = Nx*Ny;
-XY_Xr     = reshape(XY_X,XYN,1);
-XY_Yr     = reshape(XY_Y,XYN,1);
-XY_Zr     = reshape(XY_Z,XYN,1);
-XY_normBr = reshape(XY_normB,XYN,1); 
-XYC       = {'x[m]','y[m]','|B|'};
-XYBF      = num2cell([XY_Xr,XY_Zr,XY_normBr]);
-BF        = [XYC;XYBF];
-writecell(BF,'Magnitude_BFields_XY.csv');
-% XZ
-XZN       = Nx*Nz;
-XZ_Xr     = reshape(XZ_X,XZN,1);
-XZ_Yr     = reshape(XZ_Y,XZN,1);
-XZ_Zr     = reshape(XZ_Z,XZN,1);
-XZ_normBr = reshape(XZ_normB,XZN,1); 
-XZC       = {'x[m]','z[m]','|B|'};
-XZBF      = num2cell([XZ_Xr,XZ_Zr,XZ_normBr]);
-BF        = [XZC;XZBF];
-writecell(BF,'Magnitude_BFields_XZ.csv');
-% YZ
-YZN       = Nx*Nz;
-YZ_Xr     = reshape(YZ_X,YZN,1);
-YZ_Yr     = reshape(YZ_Y,YZN,1);
-YZ_Zr     = reshape(YZ_Z,YZN,1);
-YZ_normBr = reshape(YZ_normB,YZN,1); 
-YZC       = {'y[m]','z[m]','|B|'};
-YZBF      = num2cell([YZ_Xr,YZ_Zr,YZ_normBr]);
-BF        = [YZC;YZBF];
-writecell(BF,'Magnitude_BFields_YZ.csv');
+% Sx
+% Description: 
+% UNITS: [m] ARRAY: 1xNs
+%
+%
+% Sy
+% Description: 
+% UNITS: [m] ARRAY: 1xNs
+%
+%
+% Sz
+% Description: 
+% UNITS: [m] ARRAY: 1xNs
+%
+%
+% X
+% Description: 
+% UNITS: [m] ARRAY: Ny by Nx by Nz
+%
+%
+% Y
+% Description: 
+% UNITS: [m] ARRAY: Ny by Nx by Nz
+%
+%
+% Z
+% Description: 
+% UNITS: [m] ARRAY: Ny by Nx by Nz
+%
+%
+%
+% BX
+% Description: 
+% UNITS: [m] ARRAY: Ny by Nx by Nz
+%
+%
+% BY
+% Description: 
+% UNITS: [m] ARRAY: Ny by Nx by Nz
+%
+%
+% BZ
+% Description: 
+% UNITS: [m] ARRAY: Ny by Nx by Nz
+%
+%
+% nx
+% Description: 
+% UNITS: [m] ARRAY: 1x1
+%
+%
+% ny
+% Description: 
+% UNITS: [m] ARRAY: 1x1
+%
+%
+% nz
+% Description: 
+% UNITS: [m] ARRAY: 1x1
+%
+%
+% ALL
+% Description: 
+% UNITS: N/A ARRAY: 1x1 VALUES: 1 or not 1
+%
+function [] = export_BF_Ant(Sx,Sy,Sz,X,Y,Z,BX,BY,BZ,nx,ny,nz,ALL)
+% -------- WRITE: antenna points (in cartesian coordinates) --------------% 
+    %process = 'start export antenna'
+    XYZc  = {'x[m]','y[m]','z[m]'}; % strings to indicate in the output/csv 
+    % file that it is in [m]
+    aXYZ = num2cell([Sx,Sy,Sz]);
+    ANT  = [XYZc;aXYZ];
+    writecell(ANT,'wire_antenna.csv');
+    %process = 'done export antenna'
+% ------------------- WRITE: BFIELDS (ALL POINTS)------------------------ %
+    if(ALL==1)
+        %process = 'start export BF ALL'
+        [Ny,Nx,Nz] = size(X);
+        bSize = Nx*Ny*Nz;
+        BFC   = {'x[m]','y[m]','z[m]','BX','BY','BZ'};
+        % X,Y,Z,BX,BY,BZ's dim size is (Ny by Nx by Nz), therefore each needs to 
+        % become 1D array (size: Ny*Nx*Nz by 1)
+        % SPATIAL LOCATION OF BFIELDS 
+        Xcsv  = reshape(X,bSize,1);
+        Ycsv  = reshape(Y,bSize,1);
+        Zcsv  = reshape(Z,bSize,1);
+        % BFIELD VECTORS
+        BXcsv = reshape(BX,bSize,1);
+        BYcsv = reshape(BY,bSize,1);
+        BZcsv = reshape(BZ,bSize,1);
+        % convert the values into cell array
+        BFA   = num2cell([Xcsv,Ycsv,Zcsv,BXcsv,BYcsv,BZcsv]);
+        % combine cell arrays into one
+        BF    = [BFC;BFA];
+        % print/write the cell onto a csv file named, BFIELDS_ALLPOINTS
+        writecell(BF,'BFIELDS_ALLPOINTS.csv');
+        %process = 'end export BF ALL'
+    end
+% ------------------- WRITE: BFIELDS (XY-PLANE) ------------------------- %
+    %process = 'start export BF_XY'
+    
+    XYN  = Nx*Ny;   
+    BFC   = {'x[m]','y[m]','z[m]','BX','BY','BZ'};
+    % Obtain the desirable XY cut-plane
+    % reshape --> X,Y,Z,BX,BY,BZ's dim size is (Ny by Nx by Nz), therefore 
+    %each needs to become 1D array (size: Ny*Nx*Nz by 1)
+    % SPATIAL LOCATION OF BFIELDS  
+    XY_X = reshape(squeeze(X(:,:,nz)),XYN,1);
+    XY_Y = reshape(squeeze(Y(:,:,nz)),XYN,1);
+    XY_Z = reshape(squeeze(Z(:,:,nz)),XYN,1);
+    % BFIELD VECTORS
+    XY_BX = reshape(squeeze(BX(:,:,nz)),XYN,1);
+    XY_BY = reshape(squeeze(BY(:,:,nz)),XYN,1);
+    XY_BZ = reshape(squeeze(BZ(:,:,nz)),XYN,1);
+    % convert the values into cell array
+    XYBF  = num2cell([XY_X,XY_Y,XY_Z, XY_BX,XY_BY,XY_BZ]);
+    % combine cell arrays into one
+    BF    = [BFC;XYBF];
+    % print/write the cell onto a csv file named, BFIELDS_XY
+    writecell(BF,'BFIELDS_XY.csv');
+    
+    %process = 'end export BF_XY'
+    % ------------------- WRITE: BFIELDS (XZ-PLANE) ------------------------- %
+    %process = 'end export BF_XZ'
+    %tic;
+    
+    XZN  = Nx*Nz;   
+    BFC   = {'x[m]','y[m]','z[m]','BX','BY','BZ'};
+    % Obtain the desirable XZ cut-plane
+    % reshape --> X,Y,Z,BX,BY,BZ's dim size is (Ny by Nx by Nz), therefore 
+    %each needs to become 1D array (size: Ny*Nx*Nz by 1)
+    % SPATIAL LOCATION OF BFIELDS  
+    XZ_X = reshape(squeeze(X(ny,:,:)),XZN,1);
+    XZ_Y = reshape(squeeze(Y(ny,:,:)),XZN,1);
+    XZ_Z = reshape(squeeze(Z(ny,:,:)),XZN,1);
+    % BFIELD VECTORS
+    XZ_BX = reshape(squeeze(BX(ny,:,:)),XZN,1);
+    XZ_BY = reshape(squeeze(BY(ny,:,:)),XZN,1);
+    XZ_BZ = reshape(squeeze(BZ(ny,:,:)),XZN,1);
+    % convert the values into cell array
+    XZBF  = num2cell([XZ_X,XZ_Y,XZ_Z, XZ_BX,XZ_BY,XZ_BZ]);
+    % combine cell arrays into one
+    BF    = [BFC;XZBF];
+    % print/write the cell onto a csv file named, BFIELDS_XZ
+    writecell(BF,'BFIELDS_XZ.csv');
+    
+    %toc;
+    %process = 'end export BF_XZ'
+% ------------------- WRITE: BFIELDS (YZ-PLANE) ------------------------- %
+    %process = 'end export BF_YZ'
+    
+    YZN  = Ny*Nz;   
+    BFC   = {'x[m]','y[m]','z[m]','BX','BY','BZ'};
+    % Obtain the desirable YZ cut-plane
+    % reshape --> X,Y,Z,BX,BY,BZ's dim size is (Ny by Nx by Nz), therefore 
+    %each needs to become 1D array (size: Ny*Nx*Nz by 1)
+    % SPATIAL LOCATION OF BFIELDS  
+    YZ_X = reshape(squeeze(X(:,nx,:)),YZN,1);
+    YZ_Y = reshape(squeeze(Y(:,nx,:)),YZN,1);
+    YZ_Z = reshape(squeeze(Z(:,nx,:)),YZN,1);
+    % BFIELD VECTORS
+    YZ_BX = reshape(squeeze(BX(:,nx,:)),YZN,1);
+    YZ_BY = reshape(squeeze(BY(:,nx,:)),YZN,1);
+    YZ_BZ = reshape(squeeze(BZ(:,nx,:)),YZN,1);
+    % convert the values into cell array
+    YZBF  = num2cell([YZ_X,YZ_Y,YZ_Z, YZ_BX,YZ_BY,YZ_BZ]);
+    % combine cell arrays into one
+    BF    = [BFC;YZBF];
+    % print/write the cell onto a csv file named, BFIELDS_YZ
+    writecell(BF,'BFIELDS_YZ.csv');
+    
+    %process = 'end export BF_YZ'
+end
+
+
+
--- Matlab/1_Antenna_UI/new_main_UI.m
+++ Matlab/1_Antenna_UI/new_main_UI.m
@@ -196,7 +196,7 @@ view(0,90)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Nx    = 100; 
 Ny    = 100; 
-Nz    = 100; 
+Nz    = 10; 
 Ns    = [Nx,Ny,Nz];
 if (ri>=ra)
     xyz = 2*ri;
@@ -268,10 +268,7 @@ B0 = struct('BX',BX,'BY',BY,'BZ',BZ,'X',bX,'Y',bY,'Z',bZ);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% PLOT: XY PLANE
-% (working-ish)
-
-fontSize = 14;
+% ------------------- WRITE: BFIELDS (XY-PLANE) ------------------------- %
 % checks if Nz is greater than one, chooses the middle value 
 % in the case of odd number of points, round down
 if(Nz>1)
@@ -279,161 +276,14 @@ if(Nz>1)
 else
     nz = 1;
 end
-% nz = 70; % testing
-% recall that B0.? is formatted as the following, where "?" is X,Y, or Z
-% B0.?(ny,nx,nz), where n? represents the index to respecting "?"
-% Therefore if you want to plot something in the xy-plane the following
-% format is recommend to be used B0.?(:,:,nz)
-XY_X     = squeeze(B0.X(:,:,nz));
-XY_Y     = squeeze(B0.Y(:,:,nz));
-XY_Z     = squeeze(B0.Z(:,:,nz));
-Z        = zeros(Ny,Nx);
-XY_BX    = squeeze(B0.BX(:,:,nz));
-XY_BY    = squeeze(B0.BY(:,:,nz));
-XY_BZ    = squeeze(B0.BZ(:,:,nz));
-XY_normB = squeeze(normB(:,:,nz));
-%%
-%
-hold on;
-figure(1)
-% ----------- antenna -------------% 
-H=plot3(Sx,Sy,Sz,'-');
-lw = 0.5;
-set(H,'color','r'); set(H,'linewidth',lw);
-% ----------- magnitude -----------% 
-[M0_XY,c_XY]=contourf(XY_X,XY_Y,XY_normB);
-c_XY.ContourZLevel = XY_Z(1,1);
-%c_XY.ContourZLevel = 0;
-contourcbar;
-% ----------- quiver --------------%
-Q=quiver3(XY_X,XY_Y,XY_Z, XY_BX,XY_BY,XY_BZ);
-set(Q,'color','w');
-Q.MaxHeadSize = 5;
-Q.AutoScaleFactor = 5;
-% ----------- plot props -----------%
-xlabel('x[m]','FontWeight','bold','FontSize', fontSize); 
-ylabel('y[m]','FontWeight','bold','FontSize', fontSize);
-title(['XY-Plane (nz/Nz):',num2str((nz)),'/',num2str(Nz)],'FontWeight',...
-    'bold','FontSize', fontSize);
-hold on; grid on; axis tight;
-view(0,90);
-% saves the figure as a PNG 
-saveas(gcf,'MagBFields_XYPlane.png')
-hold off;
-%}
-%% PLOT: XZ PLANE
-% (working)
-if(Ny>1)
-    ny = floor(Ny/2);
-else
-    ny = 1;
-end
-ny =51; % testing
-% recall that B0.? is formatted as the following, where "?" is X,Y, or Z
-% B0.?(ny,nx,nz), where n? represents the index to respecting "?"
-% Therefore if you want to plot something in the xy-plane the following
-% format is recommend to be used B0.?(:,:,nz)
-XZ_X     = squeeze(B0.X(ny,:,:));
-XZ_Y     = squeeze(B0.Y(ny,:,:));
-XZ_Z     = squeeze(B0.Z(ny,:,:));
-Y        = zeros(Nx,Nz);
-XZ_BX    = squeeze(B0.BX(ny,:,:));
-XZ_BY    = squeeze(B0.BY(ny,:,:));
-XZ_BZ    = squeeze(B0.BZ(ny,:,:));
-XZ_normB = squeeze(normB(ny,:,:));  
-%{
-%[M0_XZ,c_XZ]=contourf(XZ_X,XZ_Z,XZ_normB);
-Q=quiver3(XZ_X,XZ_Y,XZ_Z, XZ_BX,XZ_BY,XZ_BZ);
-set(Q,'color','r');
-direction = [1 0 0];
-rotate(Q,direction,90)
-xlabel('x'); ylabel('y'); zlabel('z');
-%}
-%
-hold on;
-% ----------- antenna -------------% 
-H=plot3(Sx,Sy,Sz,'-');
-direction = [0 0 1];
-rotate(H,direction,90)
-direction = [1 0 0];
-rotate(H,direction,90)
-%lw = 5;
-set(H,'color','r'); set(H,'linewidth',lw);
-% ----------- magnitude -----------% 
-[M0_XZ,c_XZ]=contourf(XZ_X,XZ_Z,XZ_normB);
-c_XZ.ContourZLevel =  XZ_Y(1,1);
-contourcbar;
-% ----------- quiver --------------%
-Q=quiver3(XZ_X,XZ_Z,XZ_Y, XZ_BX,XZ_BY,XZ_BZ);
-Q.MaxHeadSize = 1;
-Q.AutoScaleFactor = 1.5;
-set(Q,'color','w');
-% ----------- plot props -----------%
-xlabel('x[m]','FontWeight','bold','FontSize', fontSize); 
-ylabel('z[m]','FontWeight','bold','FontSize', fontSize);
-title(['XZ-Plane (ny/Ny):',num2str((ny)),'/',num2str(Ny)],'FontWeight',...
-    'bold','FontSize', fontSize);
-grid on; 
-axis tight;
-view(0,90);
-contourcbar;
-saveas(gcf,'MagBFields_XZPlane.png')
-hold off;
-%}
-%% PLOT: YZ PLANE
-% working on
-if(Nx>1)
-    nx = floor(Nx/2);
-else
-    nx = 1;
-end
-nx = 51; % testing
-% recall that B0.? is formatted as the following, where "?" is X,Y, or Z
-% B0.?(ny,nx,nz), where n? represents the index to respecting "?"
-% Therefore if you want to plot something in the xy-plane the following
-% format is recommend to be used B0.?(:,:,nz)
-YZ_X     = squeeze(B0.X(:,nx,:));
-YZ_Y     = squeeze(B0.Y(:,nx,:));
-YZ_Z     = squeeze(B0.Z(:,nx,:));
-Z        = zeros(Ny,Nz);
-YZ_BX    = squeeze(B0.BX(:,nx,:));
-YZ_BY    = squeeze(B0.BY(:,nx,:));
-YZ_BZ    = squeeze(B0.BZ(:,nx,:));
-YZ_normB = squeeze(normB(:,nx,:));  
+% -------------------- PLOT: BFIELDS (XZ-PLANE) ------------------------- %
+% -------------------- PLOT: BFIELDS (YZ-PLANE) ------------------------- %
+nx = 25;
+ny = 25;
+nz = 25;
 
-%
-hold all;
-%figure(3)
-% ----------- antenna -------------% 
-%
-H=plot3(Sx,Sy,Sz,'-');
-direction = [0 0 1];
-rotate(H,direction,180)
-direction = [1 0 0];
-rotate(H,direction,90)
-lw = 1;
-set(H,'color','r'); set(H,'linewidth',lw);
-%}
-% ----------- magnitude -----------% 
-[M0_YZ,c_YZ]=contourf(YZ_Y,YZ_Z,YZ_normB);
-c_YZ.ContourZLevel = YZ_X(1,1);
-%c_YZ.ContourZLevel = 0;
-contourcbar;
-% ----------- quiver --------------%
-%Q=quiver3(XZ_X,XZ_Z,XZ_Y, XZ_BX,XZ_BY,XZ_BZ); %XZ
-Q=quiver3(YZ_Y,YZ_Z,YZ_X, YZ_BX,YZ_BY,YZ_BZ);
-Q.MaxHeadSize = 10;
-Q.AutoScaleFactor = 10;
-set(Q,'color','w');
-% ----------- plot props -----------%
-xlabel('y[m]','FontWeight','bold','FontSize', fontSize); 
-ylabel('z[m]','FontWeight','bold','FontSize', fontSize);
-title(['YZ-Plane (nx/Nx):',num2str((nx)),'/',num2str(Nx)],'FontWeight',...
-    'bold','FontSize', fontSize);
-view(0,90); grid on; axis tight;
-saveas(gcf,'MagBFields_YZPlane.png')
-hold off;
-%}
+plot_BF_Ant(Sx,Sy,Sz,B0.X,B0.Y,B0.Z,B0.BX,B0.BY,B0.BZ,nx,ny,nz)
+
 %%                         EXPORT CSV FILES                              %%  
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -444,8 +294,9 @@ hold off;
 % SIZE: size(Sx) by 3 UNITS: each index is [m]
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ALL = 1;
+%export_BF_Ant(Sx,Sy,Sz,B0.X,B0.Y,B0.Z,B0.BX,B0.BY,B0.BZ,nx,ny,nz, ALL);
 
-%}
 %%                         SELF-INDUCTANCE                               %%  
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % DESCRIPTION: Computes the self-inductance of an ellipse or rectangle 
