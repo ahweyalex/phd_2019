7df70ba199cdd4ac86e7ff53b03d227021b515a4
ahweyalex <ahweyalex@gmail.com>
2020-08-23 22:12:19 -0500

    Field Data with the name "exWT" contains NA so delete

--- Matlab/testing_misch_inProgress_stuff/test_selfind_Rect_Loop_N3.m
+++ Matlab/testing_misch_inProgress_stuff/test_selfind_Rect_Loop_N3.m
@@ -1,5 +1,34 @@
 clear all; close all; clc;
+ri = 10e-3;
+ra = 10e-3;
+I  = 1;
+G  = 'r';
+N  = 3;
+wT = 0.2546e-3;
+%Nm = 199; 
+%A  = importdata('Rect_N3_ri10_ra10_100.fld');
+Nm = 201; 
+Ax = importdata('Rect_ri10_ra10_100_exWT.fld');
 
+x0  = A.data(:,1);
+%y0  = A.data(:,2);
+%z0  = A.data(:,3);
+
+x  = reshape(A.data(:,1),Nm,Nm);
+%%
+y  = reshape(A.data(:,2),Nm,Nm);
+z  = reshape(A.data(:,3),Nm,Nm);
+%bx = A.data(:,4);
+%by = A.data(:,5);
+%bz = A.data(:,6);
+bx  = reshape(A.data(:,4),Nm,Nm);
+by  = reshape(A.data(:,5),Nm,Nm);
+bz  = reshape(A.data(:,6),Nm,Nm);
+[L11] = selfInductance_BFields(ri,ra,I,x,y,bz,N,G);
+
+
+% Rect_Loop_N3
+%{
 ri = 10e-3;
 ra = 10e-3;
 I  = 1;
@@ -20,3 +49,4 @@ bx  = reshape(A.data(:,4),Nm,Nm);
 by  = reshape(A.data(:,5),Nm,Nm);
 bz  = reshape(A.data(:,6),Nm,Nm);
 [L11] = selfInductance_BFields(ri,ra,I,x,y,bz,N,G);
+%}
