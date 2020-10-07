94a2fd6a16eaf2350e8b29dee230a767b81e37d2
Alexander Moreno <ahweyalex@gmail.com>
2020-10-06 22:44:31 -0500

    self indutance
    added conditions and storing arrays for the different XY res and length/radius of the wire

--- Matlab/trest3_.m
+++ Matlab/trest3_.m
@@ -7,7 +7,6 @@ u0 = 4*pi*10^-7; % free space permeability <scalar> [H/m]
 %W  = 10e-3;
 W  = 5e-3;
 %W = 2.5e-3;
-
 %for W=5e-3:5e-3:10e-3 % rect
 for W=2.5e-3:2.5e-3:5e-3 % circle
     ra = W;
@@ -62,21 +61,40 @@ for W=2.5e-3:2.5e-3:5e-3 % circle
         Y2 = squeeze(Y(:,:,1));
         %[L11(nn)] = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
         %-------------------------------rect--------------------------------------%
-        %G = 'r';
-        % N1 = 0;
-        % L11_sq_r5mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
-        % N1 = 1;
-        % L11_wT2_sq_r5mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
-        % N1 = 2;
-        % L11_notEq_sq_r5mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
+%         if(W==5e-3)
+%             G = 'r';
+%             N1 = 0;
+%             L11_sq_r5mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
+%             N1 = 1;
+%             L11_wT2_sq_r5mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
+%             N1 = 2;
+%             L11_notEq_sq_r5mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
+%         elseif(W==10e-3)
+%             G = 'r';
+%             N1 = 0;
+%             L11_sq_r10mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
+%             N1 = 1;
+%             L11_wT2_sq_r10mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
+%             N1 = 2;
+%             L11_notEq_sq_r10mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
+%         end
         %-------------------------------circ--------------------------------------%
         G = 'c';
-        N1 = 0;
-        L11_circ_r5mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
-        N1 = 1;
-        L11_wT2_circ_r5mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
-        N1 = 2;
-        L11_notEq_circ_r5mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
+        if(W==2.5e-3)
+            N1 = 0;
+            L11_circ_r5mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
+            N1 = 1;
+            L11_wT2_circ_r5mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
+            N1 = 2;
+            L11_notEq_circ_r5mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
+        elseif(W==5e-3)
+            N1 = 0;
+            L11_circ_r10mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
+            N1 = 1;
+            L11_wT2_circ_r10mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
+            N1 = 2;
+            L11_notEq_circ_r10mm(nn) = selfInductance_BFields(wT,ri,ra,I,X2,Y2,BZ,N1,G);
+        end
         nn=nn+1;
 
         if(mod(ns,100)==0)
